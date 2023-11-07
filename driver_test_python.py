8b9153ae9bf464ee6831917b70e0e6cb8ba06f13
david-villanueva-02 <david.villanueva.guzman@gmail.com>
2023-11-06 21:45:23 -0600

    drier test with python

--- driver_test_python.py
+++ driver_test_python.py
@@ -5,20 +5,27 @@ from time import sleep
 stepPin = 36
 dirPin = 38
 enPin = 40
-dir = False
 
 def Girar():
+    GPIO.output(enPin,True)      # Enables with value 0
     for x in range(400):
         GPIO.output(stepPin,True)
         sleep(0.01)
         GPIO.output(stepPin,False)
         sleep(0.01)
         print("step "+str(x)) 
-    sleep(1)
     print("Changind direction...")
-    if dir: dir = False
-    else:   dir = True
-    GPIO.output(enPin,dir)      # Enables with value 0
+    sleep(1)
+    
+    GPIO.output(enPin,False)      # Enables with value 0
+    for x in range(400):
+        GPIO.output(stepPin,True)
+        sleep(0.01)
+        GPIO.output(stepPin,False)
+        sleep(0.01)
+        print("step "+str(x)) 
+    print("Changind direction...")
+    sleep(1)
     
 GPIO.setmode(GPIO.BOARD)
 
@@ -27,7 +34,6 @@ GPIO.setup(stepPin, GPIO.OUT) # Pull Pin
 GPIO.setup(dirPin, GPIO.OUT)  # Dir Pin - controls direction
 GPIO.setup(enPin, GPIO.OUT)   # Enable pin 
 
-GPIO.output(enPin,dir)      # Enables with value 0
 GPIO.output(dirPin, True)
 
 while(True): Girar()
