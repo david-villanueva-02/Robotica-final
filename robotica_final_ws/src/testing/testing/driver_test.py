15cb38d87e6793e22537b780d1a918052db221ec
david-villanueva-02 <david.villanueva.guzman@gmail.com>
2023-11-06 21:16:11 -0600

    add step counter

--- robotica_final_ws/src/testing/testing/driver_test.py
+++ robotica_final_ws/src/testing/testing/driver_test.py
@@ -24,7 +24,8 @@ class NodeName(Node):
         GPIO.output(enPin,False)      # Enables with value 0
         GPIO.output(dirPin, True)
 
-        self.Girar()
+        while(True): self.Girar()
+        
         #self.main_timer = self.create_timer(0.001,self.callback_main_timer)
 
     def callback_main_timer(self): # 1 step - 200 steps = 1 turn
