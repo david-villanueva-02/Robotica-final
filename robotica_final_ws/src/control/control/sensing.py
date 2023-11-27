4ac1c57577e1fb8bf89c568871e277e4a439ad14
david-villanueva-02 <david.villanueva.guzman@gmail.com>
2023-11-27 13:33:48 -0600

    Un solo sensor

--- robotica_final_ws/src/control/control/sensing.py
+++ robotica_final_ws/src/control/control/sensing.py
@@ -44,7 +44,7 @@ class NodeName(Node):
         self.pub_R2 = self.create_publisher(Float32, "/R2_info", 10)
 
         self.arduino_timer = self.create_timer(0.01,self.arduino_timer_callback)
-        self.sensor1_timer = self.create_timer(0.01,self.sensor_timer_callback(16,18,2))
+        self.sensor1_timer = self.create_timer(0.01,self.sensor_timer_callback(self,16,18,2))
         #self.sensor2_timer = self.create_timer(0.01,self.sensor_timer_callback(13,15,1))
 
     # Recibe informacion de un arduino
